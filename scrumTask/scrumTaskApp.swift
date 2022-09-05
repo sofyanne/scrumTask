6e2f29cb1262d9768b4e5d48b9b21dc9ffa0f328
Sofyanne Badir <s.badir@outlook.com>
2022-09-05 15:43:59 +0200

    Initial Commit

---
+++ scrumTask/scrumTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumTaskApp.swift
+//  scrumTask
+//
+//  Created by Sofyanne Badir on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct scrumTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
